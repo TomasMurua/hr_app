9084372c90155f24a92e979f84c05ba447812ec9
TomasMurua <tomaspmurua@gmail.com>
2025-06-30 21:30:04 -0400

    W6 – T4 foto de perfil con @capacitor/camera

--- hrapp/hrapp/src/app/profile/profile.page.ts
+++ hrapp/hrapp/src/app/profile/profile.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { ProfilePhotoComponent } from '../components/profile-photo/profile-photo.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, ProfilePhotoComponent],
 })
 export class ProfilePage {
   constructor() {}
