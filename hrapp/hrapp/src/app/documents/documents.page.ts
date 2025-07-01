11a60827ab287495d09893f521f9bcde298de79b
TomasMurua <tomaspmurua@gmail.com>
2025-06-30 21:19:30 -0400

    W6 – T3 cache offline con Ionic Storage

--- hrapp/hrapp/src/app/documents/documents.page.ts
+++ hrapp/hrapp/src/app/documents/documents.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { EmployeeApiService, User } from '../services/employee-api.service';
+import { LocalDbService } from '../services/local-db.service';
+import { tap, catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-documents',
@@ -13,9 +15,15 @@ import { EmployeeApiService, User } from '../services/employee-api.service';
 export class DocumentsPage {
   users: User[] = [];
 
-  constructor(private api: EmployeeApiService) {}
+  constructor(private api: EmployeeApiService, private db: LocalDbService) {}
 
   ionViewWillEnter() {
-    this.api.getAll().subscribe((res) => (this.users = res));
+    this.api
+      .getAll()
+      .pipe(
+        tap((list) => this.db.saveUsers(list)),
+        catchError((_) => this.db.getUsers())
+      )
+      .subscribe((list) => (this.users = list || []));
   }
 }
